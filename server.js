556d55c6bca5fef0b7e1349f6f9255db7c598614
Ashley Richard <ashleymrichard@gmail.com>
2022-04-13 11:40:36 -0700

    normalize.cssand skeleton.css linked to index.ejs

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const express = require ('express');
 const  app = express ();
 const PORT = process.env.PORT || 3001;
+// const req = require("express/lib/request");
+
+app.use(express.static("public"))
 
 app.get("/", (req,res) => {
     res.send("Hello World")
 })/
 
-// app.get("/budgets", (req,res) => {
-//     res.render(index.ejs)
-// });
+app.get("/budgets", (req,res) => {
+    res.render("index.ejs")
+});
 
 app.listen(3000, () => {
     console.log(`Listening on port: ${PORT}`)
