46f9cac6d44c2d2dab1174f7af121237daee86a9
Ashley Richard <ashleymrichard@gmail.com>
2022-04-13 10:42:26 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require ('express');
+const  app = express ();
+const PORT = process.env.PORT || 3001;
+
+app.get("/", (req,res) => {
+    res.send("Hello World")
+})
+
+app.get("/budgets", (req,res) => {
+    res.render(index.ejs)
+})
+
+app.listen(3000, () => {
+    console.log(`Listening on port: ${PORT}`)
+})
