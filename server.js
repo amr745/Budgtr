72c6e198796f671804eb8603f2db8c84d376dede
Ashley Richard <ashleymrichard@gmail.com>
2022-04-13 10:53:38 -0700

    MVC File Structure Complete

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ const PORT = process.env.PORT || 3001;
 
 app.get("/", (req,res) => {
     res.send("Hello World")
-})
+})/
 
-app.get("/budgets", (req,res) => {
-    res.render(index.ejs)
-})
+// app.get("/budgets", (req,res) => {
+//     res.render(index.ejs)
+// });
 
 app.listen(3000, () => {
     console.log(`Listening on port: ${PORT}`)
-})
+});
